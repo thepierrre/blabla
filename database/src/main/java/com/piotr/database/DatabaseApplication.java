a951ce5cdffe4b6f41b9088a6ef0a0d65f756b5e
Piotr Owczarczyk <piotr@Piotrs-MBP.fritz.box>
2024-02-25 07:58:57 +0100

    add database project

---
+++ database/src/main/java/com/piotr/database/DatabaseApplication.java
@@ -0,0 +1,33 @@
+package com.piotr.database;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+@Log
+public class DatabaseApplication implements CommandLineRunner {
+
+	private final DataSource dataSource; // database object for interacting with the DB
+
+	// constructor for the DataSource object
+	public DatabaseApplication(final DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseApplication.class, args);
+	}
+
+	@Override
+	public void run(final String... args) {
+		log.info("Datasource: " + dataSource.toString());
+		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+		restTemplate.execute("select 1");
+	}
+
+}
---
+++ postgres-database/src/test/java/com/piotr/postgresdatabase/PostgresDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotr.postgresdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

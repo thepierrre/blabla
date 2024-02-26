2b7aa60e4dc2b7642a9c1479c34c2465b44dc19a
Piotr Owczarczyk <piotr@Piotrs-MBP.fritz.box>
2024-02-26 20:37:17 +0100

    add custom query

--- postgres-database/src/main/java/com/piotr/postgresdatabase/repositories/AuthorRepository.java
+++ postgres-database/src/main/java/com/piotr/postgresdatabase/repositories/AuthorRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository // bean that can be injected anywhere it's needed; you don't have to implement the interface
 public interface AuthorRepository extends CrudRepository<Author, Long> {
+    Iterable<Author> ageLessThan(int age);
 }
--- postgres-database/src/test/java/com/piotr/postgresdatabase/repositories/AuthorRepositoryIntegrationTests.java
+++ postgres-database/src/test/java/com/piotr/postgresdatabase/repositories/AuthorRepositoryIntegrationTests.java
@@ -74,4 +74,17 @@ public class AuthorRepositoryIntegrationTests {
         Optional<Author> result = underTest.findById(authorA.getId());
         assertThat(result).isEmpty();
     }
+
+    @Test
+    public void testThatGetAuthorsWithAgeLessThan() {
+        Author testAuthorA = TestDataUtil.createTestAuthorA();
+        underTest.save(testAuthorA);
+        Author testAuthorB = TestDataUtil.createTestAuthorB();
+        underTest.save(testAuthorB);
+        Author testAuthorC = TestDataUtil.createTestAuthorC();
+        underTest.save(testAuthorC);
+
+        Iterable<Author> result = underTest.ageLessThan(50);
+        assertThat(result).containsExactly(testAuthorB, testAuthorC);
+    }
 }

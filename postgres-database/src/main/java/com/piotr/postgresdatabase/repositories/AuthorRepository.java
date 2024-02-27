67265d4655e8c372e851ec8a9dbce4dc72d87af3
Piotr Owczarczyk <piotr@Piotrs-MBP.fritz.box>
2024-02-27 07:52:43 +0100

    update

--- postgres-database/src/main/java/com/piotr/postgresdatabase/repositories/AuthorRepository.java
+++ postgres-database/src/main/java/com/piotr/postgresdatabase/repositories/AuthorRepository.java
@@ -1,10 +1,14 @@
 package com.piotr.postgresdatabase.repositories;
 
 import com.piotr.postgresdatabase.domain.Author;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository // bean that can be injected anywhere it's needed; you don't have to implement the interface
 public interface AuthorRepository extends CrudRepository<Author, Long> {
     Iterable<Author> ageLessThan(int age);
+
+    @Query("SELECT a from Author a where a.age > ?1")
+    Iterable<Author> findAuthorsWithAgeGreaterThan(int age);
 }
--- postgres-database/src/test/java/com/piotr/postgresdatabase/repositories/AuthorRepositoryIntegrationTests.java
+++ postgres-database/src/test/java/com/piotr/postgresdatabase/repositories/AuthorRepositoryIntegrationTests.java
@@ -87,4 +87,17 @@ public class AuthorRepositoryIntegrationTests {
         Iterable<Author> result = underTest.ageLessThan(50);
         assertThat(result).containsExactly(testAuthorB, testAuthorC);
     }
+
+    @Test
+    public void testThatGetAuthorsWithAgeGreaterThan() {
+        Author testAuthorA = TestDataUtil.createTestAuthorA();
+        underTest.save(testAuthorA);
+        Author testAuthorB = TestDataUtil.createTestAuthorB();
+        underTest.save(testAuthorB);
+        Author testAuthorC = TestDataUtil.createTestAuthorC();
+        underTest.save(testAuthorC);
+
+        Iterable<Author> result = underTest.findAuthorsWithAgeGreaterThan(50);
+        assertThat(result).containsExactly(testAuthorA);
+    }
 }
